2ccc19e1d979f38e1622287f7fa1d77ac4057879
unstopablewoman10 <hewitek@gmail.com>
2023-01-26 13:22:53 -0800

    updated

--- handle_print.c
+++ handle_print.c
@@ -1,50 +1,40 @@
 #include "main.h"
-/**
- *  * handle_print - Prints an argument based on its type
- *   * @fmt: Formatted string in which to print the arguments.
- *    * @list: List of arguments to be printed.
- *     * @ind: ind.
- *      * @buffer: Buffer array to handle print.
- *       * @flags: Calculates active flags
- *        * @width: get width.
- *         * @precision: Precision specification
- *          * @size: Size specifier
- *           * Return: 1 or 2;
- *            */
-int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
-			int flags, int width, int precision, int size)
-{
-		int i, unknow_len = 0, printed_chars = -1;
-			fmt_t fmt_types[] = {
-						{'c', print_char}, {'s', print_string}, {'%', print_percent},
-								{'i', print_int}, {'d', print_int}, {'b', print_binary},
-										{'u', print_unsigned}, {'o', print_octal}, {'x', print_hexadecimal},
-												{'X', print_hexa_upper}, {'p', print_pointer}, {'S', print_non_printable},
-														{'r', print_reverse}, {'R', print_rot13string}, {'\0', NULL}
-							};
-				for (i = 0; fmt_types[i].fmt != '\0'; i++)
-							if (fmt[*ind] == fmt_types[i].fmt)
-											return (fmt_types[i].fn(list, buffer, flags, width, precision, size));
 
-					if (fmt_types[i].fmt == '\0')
-							{
-										if (fmt[*ind] == '\0')
-														return (-1);
-												unknow_len += write(1, "%%", 1);
-														if (fmt[*ind - 1] == ' ')
-																		unknow_len += write(1, " ", 1);
-																else if (width)
-																			{
-																							--(*ind);
-																										while (fmt[*ind] != ' ' && fmt[*ind] != '%')
-																															--(*ind);
-																													if (fmt[*ind] == ' ')
-																																		--(*ind);
-																																return (1);
-																																		}
-																		unknow_len += write(1, &fmt[*ind], 1);
-																				return (unknow_len);
-																					}
-						return (printed_chars);
+/**
+ * get_print - selects the right printing function
+ * depending on the conversion specifier passed to _printf
+ * @s: character that holds the conversion specifier
+ * Description: the function loops through the structs array
+ * func_arr[] to find a match between the specifier passed to _printf
+ * and the first element of the struct, and then the approriate
+ * printing function
+ * Return: a pointer to the matching printing function
+ */
+int (*get_print(char s))(va_list, flags_t *)
+{
+	ph func_arr[] = {
+		{'i', print_int},
+		{'s', print_string},
+		{'c', print_char},
+		{'d', print_int},
+		{'u', print_unsigned},
+		{'x', print_hex},
+		{'X', print_hex_big},
+		{'b', print_binary},
+		{'o', print_octal},
+		{'R', print_rot13},
+		{'r', print_rev},
+		{'S', print_bigS},
+		{'p', print_address},
+		{'%', print_percent}
+		};
+	int flags = 14;
+
+	register int i;
+
+	for (i = 0; i < flags; i++)
+		if (func_arr[i].c == s)
+			return (func_arr[i].f);
+	return (NULL);
 }
 
--- utils.c
+++ utils.c
@@ -1,86 +1,34 @@
 #include "main.h"
 
 /**
- *  * is_printable - Evaluates if a char is printable
- *   * @c: Char to be evaluated.
- *    *
- *     * Return: 1 if c is printable, 0 otherwise
- *      */
-int is_printable(char c)
+ * print_string - loops through a string and prints
+ * every character
+ * @l: va_list arguments from _printf
+ * @f: pointer to the struct flags that determines
+ * if a flag is passed to _printf
+ * Return: number of char printed
+ */
+int print_string(va_list l, flags_t *f)
 {
-		if (c >= 32 && c < 127)
-					return (1);
+	char *s = va_arg(l, char *);
 
-			return (0);
+	(void)f;
+
+	if (!s)
+		s = "(null)";
+	return (_puts(s));
 }
 
 /**
- *  * append_hexa_code - Append ascci in hexadecimal code to buffer
- *   * @buffer: Array of chars.
- *    * @i: Index at which to start appending.
- *     * @ascii_code: ASSCI CODE.
- *      * Return: Always 3
- *       */
-int append_hexa_code(char ascii_code, char buffer[], int i)
+ * print_char - prints a character
+ * @l: va_list arguments from _printf
+ * @f: pointer to the struct flags that determines
+ * if a flag is passed to _printf
+ * Return: number of char printed
+ */
+int print_char(va_list l, flags_t *f)
 {
-		char map_to[] = "0123456789ABCDEF";
-			/* The hexa format code is always 2 digits long */
-			if (ascii_code < 0)
-						ascii_code *= -1;
-
-				buffer[i++] = '\\';
-					buffer[i++] = 'x';
-
-						buffer[i++] = map_to[ascii_code / 16];
-							buffer[i] = map_to[ascii_code % 16];
-
-								return (3);
-}
-
-/**
- *  * is_digit - Verifies if a char is a digit
- *   * @c: Char to be evaluated
- *    *
- *     * Return: 1 if c is a digit, 0 otherwise
- *      */
-int is_digit(char c)
-{
-		if (c >= '0' && c <= '9')
-					return (1);
-
-			return (0);
-}
-
-/**
- *  * convert_size_number - Casts a number to the specified size
- *   * @num: Number to be casted.
- *    * @size: Number indicating the type to be casted.
- *     *
- *      * Return: Casted value of num
- *       */
-long int convert_size_number(long int num, int size)
-{
-		if (size == S_LONG)
-					return (num);
-			else if (size == S_SHORT)
-						return ((short)num);
-
-				return ((int)num);
-}
-
-/**
- *  * convert_size_unsgnd - Casts a number to the specified size
- *   * @num: Number to be casted
- *    * @size: Number indicating the type to be casted
- *     *
- *      * Return: Casted value of num
- *       */
-long int convert_size_unsgnd(unsigned long int num, int size)
-{
-		if (size == S_LONG)
-					return (num);
-			else if (size == S_SHORT)
-						return ((unsigned short)num);
-
-				return ((unsigned int)num);
+	(void)f;
+	_putchar(va_arg(l, int));
+	return (1);
 }
