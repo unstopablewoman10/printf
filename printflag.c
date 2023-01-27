716da692a7e57405ea213cfd0b04a906df894a7a
unstopablewoman10 <hewitek@gmail.com>
2023-01-27 13:22:39 -0800

    updated

--- printflag.c
+++ printflag.c
@@ -1,6 +1,8 @@
 #include "main.h"
+
 /**
  * get_flag - turns on flags if _printf finds
+ * a flag modifier in the format string
  * @s: character that holds the flag specifier
  * @f: pointer to the struct flags in which we turn the flags on
  *
@@ -8,21 +10,24 @@
  */
 int get_flag(char s, flags_t *f)
 {
-int i = 0;
-switch (s)
-{
-case '+':
-f->plus = 1;
-i = 1;
-break;
-case ' ':
-f->space = 1;
-i = 1;
-break;
-case '#':
-f->hash = 1;
-i = 1;
-break;
+	int i = 0;
+
+	switch (s)
+	{
+		case '+':
+			f->plus = 1;
+			i = 1;
+			break;
+		case ' ':
+			f->space = 1;
+			i = 1;
+			break;
+		case '#':
+			f->hash = 1;
+			i = 1;
+			break;
+	}
+
+	return (i);
 }
-return (i);
-}
+
